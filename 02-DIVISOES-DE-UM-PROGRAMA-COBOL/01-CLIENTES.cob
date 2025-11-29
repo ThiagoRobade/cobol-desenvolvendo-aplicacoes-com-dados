81d64d09a2a1ce76ebe05d86c299f43b98b488ee
ThiagoRobade <robade@live.com>
2025-11-29 18:57:51 -0300

    chore: initial COBOL program structure for CLIENTES - Added basic COBOL program with IDENTIFICATION, ENVIRONMENT, DATA, and PROCEDURE divisions - Included initial CLIENTES output for testing - Added placeholder comment for future documentation

---
+++ 02-DIVISOES-DE-UM-PROGRAMA-COBOL/01-CLIENTES.cob
@@ -0,0 +1,8 @@
+       IDENTIFICATION          DIVISION.
+       PROGRAM-ID. 01-CLIENTES.
+      * COMENTARIO 
+       ENVIRONMENT             DIVISION.
+       DATA                    DIVISION.
+       PROCEDURE               DIVISION.
+           DISPLAY "CLIENTES".
+           STOP RUN.
